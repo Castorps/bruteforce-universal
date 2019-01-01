7c8c2d000e47a14b9c816bb143aef94336b135ef
Castorp <phohgh7Ve5u@protonmail.com>
2019-01-01 22:02:35 +0000

    Update const.py

--- lib/const.py
+++ lib/const.py
@@ -4,19 +4,20 @@
 combos_max = 1500000  # maximum number of combos to load into memory
 
 # Proxy Manager
-proxies_minimum = 1500  # if proxy list is smaller, proxies get scraped
-proxy_ban_time = 30  # time to wait before using proxy again if it has been banned
-proxy_minimum_attempts = 5  # minimum attempts proxy has to do before calculating success ratio
+proxies_minimum = 1000  # if proxy list is smaller, scrape proxies
+proxy_ban_time = 30  # time to wait after proxy ban to use it again
+proxy_minimum_attempts = 3  # minimum attempts proxy has to do before calculating success ratio
 proxy_success_ratio = 1.5  # = tested / retries
 proxy_timeout = 5  # time to wait before using proxy again
 
 # Browser
 auth_ssl = True
 connection_timeout = 7
-header_items = []  # <header_item_name>;<header_item_value>
-header_items_cookies = ['X-CSRFToken;csrftoken']  # <header_item_name>;<cookie_name> (value is cookie's value)
-home_url = 'https://www.instagram.com/'
-login_url = 'https://www.instagram.com/accounts/login/ajax/'
-post_data = {'username': '{username}', 'password': '{password}'}  # use {username} and {password}
-response_success = '"authenticated": true'  # if this is somewhere in response, credentials worked
-response_error = 'error'  # if this is somewhere in response, there was an error
+headers = {}  # {<header_name>: <header_value>}
+headers_cookies = {'X-CSRFToken': 'csrftoken'}  # {<header_name>: <cookie_name>} (value will be cookie's value)
+home_url = 'https://www.instagram.com/'  # used to get cookies
+login_url = 'https://www.instagram.com/accounts/login/ajax/'  # used for authentication
+payload = {'username': '{username}', 'password': '{password}'}  # {<field_name>: <field_value>}, use {username/password}
+response_success = '"authenticated": true'  # if this is found in the response, authentication succeeded
+response_error = 'error'  # if this is found in the response, there was an error
+
