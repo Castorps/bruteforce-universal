9e0d96304b65ee0b72f22ed9f944ae325e4986e6
Castorp <phohgh7Ve5u@protonmail.com>
2019-01-04 05:24:58 +0000

    Update const.py

--- module/const.py
+++ module/const.py
@@ -4,10 +4,10 @@
 combos_max = 2000000  # maximum number of combos to load into memory
 
 # Proxy Manager
-proxies_minimum = 1000  # if proxy list is smaller, scrape proxies
+proxies_minimum = 500  # if proxy list is smaller, scrape proxies
 proxy_ban_time = 30  # time to wait after proxy ban to use it again
-proxy_minimum_attempts = 3  # minimum attempts proxy has to do before calculating success ratio
-proxy_success_ratio = 1.5  # = tested / retries
+proxy_minimum_attempts = 4  # minimum attempts proxy has to do before calculating success ratio
+proxy_success_ratio = 1  # = tested / retries
 proxy_timeout = 5  # time to wait before using proxy again
 
 # Browser
