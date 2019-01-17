7141ddb8e3602fcc00e63af53ac0baa6255043ab
Castorp <phohgh7Ve5u@protonmail.com>
2019-01-17 13:43:50 +0000

    Optimized configuration

--- module/const.py
+++ module/const.py
@@ -4,15 +4,15 @@
 combos_max = 2000000  # maximum number of combos to load into memory
 
 # Proxy Manager
-proxies_minimum = 500  # if proxy list is smaller, scrape proxies
+proxies_minimum = 250  # if proxy list is smaller, scrape proxies
 proxy_ban_time = 30  # time to wait after proxy ban to use it again
 proxy_minimum_attempts = 4  # minimum attempts proxy has to do before calculating success ratio
 proxy_success_ratio = 1  # = tested / retries
-proxy_timeout = 5  # time to wait before using proxy again
+proxy_timeout = 10  # time to wait before using proxy again
 
 # Browser
 auth_ssl = True
-connection_timeout = 7
+connection_timeout = 10
 headers = {}  # {<header_name>: <header_value>}
 headers_cookies = {'X-CSRFToken': 'csrftoken'}  # {<header_name>: <cookie_name>} (value will be cookie's value)
 home_url = 'https://www.instagram.com/'  # used to get cookies
