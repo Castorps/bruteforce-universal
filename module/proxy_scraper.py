78f6a02f6d7b1ae840d5df3f10b0dca46c503a1f
Castorp <phohgh7Ve5u@protonmail.com>
2019-01-23 17:05:05 +0000

    Update proxy_scraper.py

--- module/proxy_scraper.py
+++ module/proxy_scraper.py
@@ -1,12 +1,15 @@
 import re
+import ssl
 import urllib.request
 
 
 def get_sourcecode(url):
     try:
+        ssl._create_default_https_context = ssl._create_unverified_context
         request = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
         request_source = urllib.request.urlopen(request).read()
         return str(request_source)
+    
     except:
         return None
 
@@ -16,7 +19,7 @@ def scrape_table_proxies(url):
     source = get_sourcecode(url)
     
     if source:
-        re_proxy = re.compile('\d{1,6}:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+        re_proxy = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,6}')
         source = source.replace('&lt;', '<').replace('&gt;', '>').replace('</td><td>', ':')
         proxies = re_proxy.findall(source)
 
@@ -25,26 +28,26 @@ def scrape_table_proxies(url):
 
 class ProxyScraper:
 
-    def __init__(self, input_proxy_file):
+    def __init__(self):
         self.proxies = set()
-        self.proxy_file = input_proxy_file
         
     def scrape(self):
         proxies = []
         
         sslproxies_org = scrape_table_proxies('https://www.sslproxies.org/')
-        freeproxylist_net = scrape_table_proxies('https://free-proxy-list.net')
+        freeproxylist_net = scrape_table_proxies('https://free-proxy-list.net/')
         freeproxylist_net_anon = scrape_table_proxies('https://free-proxy-list.net/anonymous-proxy.html')
         spys_me = scrape_table_proxies('http://spys.me/proxy.txt')
 
-        proxies += [sslproxies_org
+        proxies += (sslproxies_org
                    + freeproxylist_net
                    + freeproxylist_net_anon
-                   + spys_me]
-        
+                   + spys_me)
+
         self.proxies = set(proxies)
 
     def get(self):
         proxies = self.proxies
         self.proxies = set()
         return proxies
+    
