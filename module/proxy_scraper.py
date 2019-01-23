46605ecbe03f03c43dc7eb53111bd58028c9bf14
Castorp <phohgh7Ve5u@protonmail.com>
2019-01-23 16:41:55 +0000

    Update proxy_scraper.py

--- module/proxy_scraper.py
+++ module/proxy_scraper.py
@@ -1,3 +1,26 @@
+import re
+import urllib.request
+
+
+def get_sourcecode(url):
+    try:
+        request = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        request_source = urllib.request.urlopen(request).read()
+        return str(request_source)
+    except:
+        return None
+
+    
+def scrape_table_proxies(url):
+    proxies = []
+    source = get_sourcecode(url)
+    
+    if source:
+        re_proxy = re.compile('\d{1,6}:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+        source = source.replace('&lt;', '<').replace('&gt;', '>').replace('</td><td>', ':')
+        proxies = re_proxy.findall(source)
+
+    return proxies
 
 
 class ProxyScraper:
@@ -5,16 +28,20 @@ class ProxyScraper:
     def __init__(self, input_proxy_file):
         self.proxies = set()
         self.proxy_file = input_proxy_file
-
+        
     def scrape(self):
         proxies = []
+        
+        sslproxies_org = scrape_table_proxies('https://www.sslproxies.org/')
+        freeproxylist_net = scrape_table_proxies('https://free-proxy-list.net')
+        freeproxylist_net_anon = scrape_table_proxies('https://free-proxy-list.net/anonymous-proxy.html')
+        spys_me = scrape_table_proxies('http://spys.me/proxy.txt')
 
-        with open(self.proxy_file, 'r', encoding='utf-8', errors='ignore') as proxy_file:
-            for proxy in proxy_file:
-                if ':' in proxy:
-                    proxy = proxy.replace('\n', '').replace('\r', '').replace('\t', '')
-                    proxies.append(proxy)
-
+        proxies += [sslproxies_org
+                   + freeproxylist_net
+                   + freeproxylist_net_anon
+                   + spys_me]
+        
         self.proxies = set(proxies)
 
     def get(self):
