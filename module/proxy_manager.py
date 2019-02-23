9cb055464ccf450499474df043e71b304082a8ba
Castorp <phohgh7Ve5u@protonmail.com>
2019-02-23 13:49:53 +0000

    Overall change: classes reworked
    proxy manager creates it's own thread to manage proxy list;
    new proxies will only be added to the proxies_ready deque, if they're not in it already;
    added and changed a few comments;

--- module/proxy_manager.py
+++ module/proxy_manager.py
@@ -1,5 +1,6 @@
 from .const import (proxy_ban_time, proxy_minimum_attempts, proxy_success_ratio, proxy_timeout)
 from collections import deque
+from threading import Thread
 from time import (sleep, time)
 
 
@@ -8,6 +9,7 @@ class ProxyManager:
     def __init__(self):
         self.proxies = {}  # see below; holds all proxies and there parameters
         self.proxies_ready = deque()  # see below; holds ready proxies and authentication parameters
+        self.maintain_thread = None
         self.isAlive = True
 
     @property
@@ -15,7 +17,8 @@ class ProxyManager:
         return len(self.proxies)
 
     def put(self, proxy_list):
-        # add proxies to self.proxies
+        
+        # add proxies from proxy_list ([ip:port:type:username:password, ...]; ip:port mandatory) to self.proxies
         # parse ip, port, type, username, password
         for proxy in proxy_list:
             proxy_parts = proxy.split(':')
@@ -40,24 +43,25 @@ class ProxyManager:
             proxy_type = proxy_type.lower()
                 
             if proxy_type not in ['http', 'socks5', 'socks5h']:
-                proxy_type = 'http'
+                proxy_type = 'http' 
 
-            self.proxies[proxy_ip + ':' + proxy_port] = [proxy_type,   # type 
-                                                         proxy_username,   # username
+            self.proxies[proxy_ip + ':' + proxy_port] = [proxy_type,  # type
+                                                         proxy_username,  # username
                                                          proxy_password,   # password
                                                          0,   # tested
                                                          0,   # retries
                                                          None,   # disabled timestamp
                                                          False]  # ban flag
 
-            # add proxy to pool of 'ready' proxies
-            self.proxies_ready.append([proxy_ip + ':' + proxy_port, 
-                                       proxy_type, 
-                                       proxy_username, 
-                                       proxy_password])
+            # add proxy to pool of 'ready' proxies if it's not in there already
+            if [proxy_ip + ':' + proxy_port, proxy_type, proxy_username, proxy_password] not in self.proxies_ready:
+                self.proxies_ready.append([proxy_ip + ':' + proxy_port,
+                                           proxy_type,
+                                           proxy_username,
+                                           proxy_password])
 
     def get(self):
-        while len(self.proxies_ready) == 0:
+        while not len(self.proxies_ready) and self.isAlive:
             sleep(0.5)
             
         return self.proxies_ready.popleft()
@@ -75,16 +79,13 @@ class ProxyManager:
 
             if ban_flag:
                 proxy_stats[6] = True
-                
+
         except KeyError:
             return
 
-    def stop(self):
-        self.isAlive = False
-
-    def start(self):
+    def maintain(self):
         while self.isAlive:
-            
+
             # list avoids changing dictionary during loop
             for proxy in list(self.proxies):
                 proxy_stats = self.proxies[proxy]
@@ -102,18 +103,27 @@ class ProxyManager:
                     # banned or disabled? => set wait time
                     if proxy_stats[6]:
                         time_limit = proxy_ban_time
+
                     else:
                         time_limit = proxy_timeout
 
-                    # reenable proxy if wait time has passed
+                    # enable proxy if wait time has passed
                     if (time() - proxy_stats[5]) >= time_limit:
                         proxy_stats[5] = None
                         proxy_stats[6] = False
 
-                        # add proxy to pool of 'ready' proxies
                         self.proxies_ready.append([proxy,
                                                    proxy_stats[0],
                                                    proxy_stats[1],
                                                    proxy_stats[2]])
 
             sleep(0.5)
+
+    def stop(self):
+        self.isAlive = False
+        self.maintain_thread.join()
+
+    def start(self):
+        self.isAlive = True
+        self.maintain_thread = Thread(target=self.maintain)
+        self.maintain_thread.start()
