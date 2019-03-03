3fa64cc2810a3a3a0db22b28d82c83c426993adc
Castorp <phohgh7Ve5u@protonmail.com>
2019-03-03 23:25:56 +0000

    Update browser.py

--- module/browser.py
+++ module/browser.py
@@ -1,5 +1,6 @@
-from .const import (auth_ssl, connection_timeout, headers, headers_cookies, home_url, login_url, payload)
+from .const import (auth_ssl, connection_timeout, headers, headers_cookies, home_url, login_url, payload, payload_json, payload_put)
 from requests import Session
+from json import dumps
 
 
 class Browser:
@@ -16,7 +17,6 @@ class Browser:
 
         if auth_ssl:
             self.auth_type = 'https'
-
         else:
             self.auth_type = 'http'
 
@@ -26,7 +26,6 @@ class Browser:
         # set authentication by look at username and password of proxy
         if not proxy[2] or not proxy[3]:
             proxy_auth = ''
-
         else:
             proxy_auth = proxy[2] + ':' + proxy[3] + '@'
 
@@ -37,7 +36,7 @@ class Browser:
         self.cookies = self.session.get(home_url, timeout=connection_timeout).cookies.get_dict()
 
     def set_header(self):
-        if len(headers_cookies):
+        if len(home_url):
             self.get_cookies()
 
         # include cookie values in header (=dictionary)
@@ -45,12 +44,22 @@ class Browser:
             if cookie_name in self.cookies:
                 headers[header_name] = self.cookies[cookie_name]
 
+        # build_payload has to be called first, if payload is supposed to be json
+        for field in headers:
+            headers[field] = headers[field].format(content_length=str(len(self.payload)))
+
         self.session.headers.update(headers)
 
     def build_payload(self):
         for field in payload:
-            self.payload[field] = payload[field].format(username=self.combo[0], password=self.combo[1])
+            self.payload[field] = str(payload[field]).format(username=self.combo[0], password=self.combo[1])
+
+        if payload_json:
+            self.payload = dumps(self.payload).encode('utf-8')
 
     def post(self):
-        response = self.session.post(login_url, data=self.payload, timeout=connection_timeout)
+        if payload_put:
+            response = self.session.put(login_url, data=self.payload, timeout=connection_timeout)
+        else:
+            response = self.session.post(login_url, data=self.payload, timeout=connection_timeout)
         return response
